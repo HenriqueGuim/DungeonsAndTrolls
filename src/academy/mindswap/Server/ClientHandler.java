da7799c182509fb6a98ecd79af7ae6411619b112
Henrique Guimarães <zpatins@gmail.com>
2022-08-19 10:40:49 +0100

    Created Server

---
+++ src/academy/mindswap/Server/ClientHandler.java
@@ -0,0 +1,69 @@
+package academy.mindswap.Server;
+
+import java.io.*;
+import java.net.Socket;
+
+public class ClientHandler {
+    private Socket playerSocket;
+    private BufferedReader reader;
+    private BufferedWriter writer;
+    private String name;
+    private boolean isPlaying = false;
+    public ClientHandler(Socket accept) {
+        this.playerSocket = accept;
+        startBuffers();
+    }
+
+    private void startBuffers() {
+        try {
+            reader = new BufferedReader( new InputStreamReader(playerSocket.getInputStream()));
+            writer = new BufferedWriter(new OutputStreamWriter(playerSocket.getOutputStream()));
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean isOffline(){
+        return playerSocket.isClosed();
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String readMessage() {
+        try {
+            String message = reader.readLine();
+            if (message == null) {
+                playerSocket.close();
+            }
+            return message;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void sendMessage(String message) {
+        try {
+            writer.write(message);
+            writer.newLine();
+            writer.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean isPlaying() {
+        return isPlaying;
+    }
+    public void startGame(){
+        isPlaying = true;
+    }
+    public void endGame(){
+        isPlaying = false;
+    }
+}
